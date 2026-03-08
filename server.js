341910c8d3f2c92299c07b0ca452613c30829f0b
Michael Will <michaelwill326@gmail.com>
2026-03-07 17:40:53 -0700

    Add Cloudinary config

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ const mongoose = require("mongoose")
 const axios = require("axios")
 const nodemailer = require("nodemailer")
 
+const multer = require("multer")
+const { CloudinaryStorage } = require("multer-storage-cloudinary")
+const cloudinary = require("cloudinary").v2
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+})
 const http = require("http")
 const {Server} = require("socket.io")
 
